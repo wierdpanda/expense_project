32b55b860c162af7e6f86a9ebfadf2794ea005e0
wierdpanda <60147367+wierdpanda@users.noreply.github.com>
2026-06-01 20:55:33 +0200

    multiple fixes and changes
    -Moved some old code files backup files from before adding project to GitHub to a folder for reference
    
    -Changed "Do you wish to clear old save data?" to "Do you wish to load old save data?" as most people would press y by default and loose their data.
    
    -Added comments for easier readability
    
    -Changed some printed text to match  the added catagory vairable/values taht are being implemented
    
    -Changed length  of expense[ ] check from 2 values to 3
    
    -Added Category to expense list
    
    -Added a choice in view total expenses to view by category (still to be implemented)  and view total expenses

--- expense.py
+++ expense.py
@@ -13,8 +13,9 @@ SAVE_FILE = "save_state_expense.json"
 # =====================
 
 while True:
-    choice = input("Do you wish to clear previous save data? (Y/N) ").lower()
-    if choice == "y":
+    choice = input("Do you wish to load previous save data? (Y/N) " \
+    "\nNote failure to load previous data will result in deleting old data and starting from scratch\n").lower()
+    if choice == "n":
         if os.path.exists(SAVE_FILE):
             os.remove(SAVE_FILE)
             print("🗑️ Save data has been deleted.")
@@ -22,7 +23,7 @@ while True:
         else:
             print("ℹ️ No save file to delete.")
             break
-    elif choice =="n":
+    elif choice =="y":
         print("Continueing with existing data.")
         break
     else:
@@ -57,9 +58,12 @@ def save_state(expenses):
     print(f"💾 State saved to {os.path.abspath(SAVE_FILE)}")
 
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    # the above save does not work
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# Main program start
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 
 #1 list within list to link variables together
 expenses = load_state()
@@ -68,28 +72,42 @@ expenses = load_state()
 #Outer loop for main program
 while True:
     print("\nWelcome to Nat's expense tracker")
-    print("1. Add expense name + amount")
+    print("1. Add expense name, amount and Category")
     print("2. View expenses")
     print("3. View total spent")
     print("4. Exit program")
 
     choice = input("Please select an option: ")
-    #inner loop for adding a name + expense amount
+
+
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    #inner loop for adding a name + expense amount + category
+
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     if choice == "1":
         while True:
-            user_input = input("\nEnter expense name and amount (or q to return to previous menu): ")
+            user_input = input("\nEnter expense name, amount and category (or q to return to previous menu): ")
             #back to previous menu input
             if user_input == "q":
                 break
 
             parts = user_input.split()
             #name and amount input
-            if len(parts) != 2:
-                print("\nPlease enter exactly 2 values (item amount)")
+            if len(parts) != 3:
+                print("\nPlease enter exactly 3 values (item amount)")
                 continue
 
             name = parts[0]
             amount = parts[1]
+            category = parts[2]
+
+            #>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+            #Rules for amount  inputs  START
+
+            #>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
             #ensure amount input is a number
             if not amount.lstrip("-").isdigit():
                 print("\nAmount must be a number")
@@ -99,21 +117,41 @@ while True:
             if amount <=0:
                 print("\nAmount must be above 0")
                 continue
-            #putting both amounts into 1 list
-            expenses.append([name, int(amount)])
 
-            print(f"\nAdded {name} with amount R{amount}")
+        
+
+
+            #putting both variables into 1 list
+
+            expenses.append([name, int(amount), category])
+
+            print(f"\nAdded {name} with amount R{amount} in the {category} category")
     #choice 2 to view all expenses under each other
     elif choice == "2":
         print("\nExpenses:")
         for expense in expenses:
-            print(f"{expense[0]} - R{expense[1]}")
-    #view the total amount for al expenses     
+            print(f"{expense[0]} - R{expense[1]} on {expense[2]}")
+
+
+
+    #view the total amount for alL expenses     
     elif choice == "3":
         total = 0
-        for expense in expenses:
-            total += expense[1]
-        print(f"\nTotal spent: R{total}")
+
+        print("\n1. Total for each category")
+        print("2. Total for all expenses")
+
+        expense_choice=input("Please select an option:")
+
+        #each category with its total expense
+        if expense_choice == "1":
+            print("We are still working on this option please select option 2 until we have implemented this feature")
+
+        elif expense_choice == "2":    
+            for expense in expenses:
+                total += expense[1]
+            print(f"\nTotal spent: R{total} \n")
+            print("Returning to main menu.\n")
     #this closes the program
     elif choice == "4":
         save_state(expenses)
